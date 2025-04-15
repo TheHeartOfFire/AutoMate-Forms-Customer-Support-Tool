29c6fabfff47decde26ce6cbc46843a765fd587a
Dakota Jordan <heartoffiremedia@yahoo.com>
2025-04-15 16:55:56 -0500

    View Model update for Dashboard page.
    All fields boxes now have a backing field. Just need to handle interaction logic in the view model.

--- AMFormsCST.Desktop/ViewModels/Pages/DashboardViewModel.cs
+++ AMFormsCST.Desktop/ViewModels/Pages/DashboardViewModel.cs
@@ -11,4 +11,61 @@ public partial class DashboardViewModel : ViewModel
 
     [ObservableProperty]
     private Note? _selectedNote;
+
+    [ObservableProperty]
+    private ObservableCollection<Note> _dealers = [];
+
+    [ObservableProperty]
+    private Note? _selectedDealer;
+
+    [ObservableProperty]
+    private ObservableCollection<Note> _companies = [];
+
+    [ObservableProperty]
+    private Note? _selectedCompany;
+
+    [ObservableProperty]
+    private ObservableCollection<Note> _forms = [];
+
+    [ObservableProperty]
+    private Note? _selectedForm;
+
+    [ObservableProperty]
+    private string _companyName = string.Empty;
+
+    [ObservableProperty]
+    private string _serverId = string.Empty;
+
+    [ObservableProperty]
+    private string _companyNumber = string.Empty;
+
+    [ObservableProperty]
+    private string _contactName = string.Empty;
+
+    [ObservableProperty]
+    private string _contactEmail = string.Empty;
+
+    [ObservableProperty]
+    private string _contactPhone = string.Empty;
+
+    [ObservableProperty]
+    private string _contactPhoneDelimiter = string.Empty;
+
+    [ObservableProperty]
+    private string _contactPhoneExtension = string.Empty;
+
+    [ObservableProperty]
+    private string _formName = string.Empty;
+
+    [ObservableProperty]
+    private string _formTestDeal = string.Empty;
+
+    [ObservableProperty]
+    private string _formNotes = string.Empty;
+
+    [ObservableProperty]
+    private string _caseNumber = string.Empty;
+
+    [ObservableProperty]
+    private string _generalNotes = string.Empty;
 }

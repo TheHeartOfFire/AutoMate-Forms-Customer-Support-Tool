f47c954fd672b48929f40a136dd74ca1c7841cf9
Dakota Jordan <heartoffiremedia@yahoo.com>
2025-07-25 18:50:35 -0500

    Established navigation to this page and set up an empty window to begin implementing the templates in.

---
+++ AMFormsCST.Desktop/Views/Pages/Tools/TemplatesPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using AMFormsCST.Desktop.ControlsLookup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Ui.Controls;
+
+namespace AMFormsCST.Desktop.Views.Pages.Tools;
+/// <summary>
+/// Interaction logic for TemplatesPage.xaml
+/// </summary>
+[GalleryPage("This utility allows you to manage your text templates which references the other parts of this program to prefill data", SymbolRegular.MailTemplate24)]
+public partial class TemplatesPage : Page
+{
+    public TemplatesPage()
+    {
+        InitializeComponent();
+    }
+}
